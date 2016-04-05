a463f13b51262bc76b203537de362044535e78aa
Camcan <mr.campbell.hawkes@gmail.com>
2016-04-05 15:26:09 +1200

    2/11

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,1 +1,12 @@
-console.log("HELLO WORLD")
+var numbers = process.argv
+
+function outputSum(array){
+  var sum = 0
+  for (n=2; n < array.length; n++) {
+    sum += parseInt(array[n])
+  }
+  console.log(sum)
+}
+
+outputSum(numbers)
+
