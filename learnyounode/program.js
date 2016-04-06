475a2235990de32a97643892a62e735318f3074c
Camcan <mr.campbell.hawkes@gmail.com>
2016-04-06 23:47:04 +1200

    #10 Time Server - Output not matching. To be resolved. Task completes successfully.

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -5,26 +5,28 @@ var port = process.argv[2]
 
 console.log(port)
 var net = require('net')
-var server = net.createServer(function (socket) {
- var date = new Date
- date = [
-   date.getFullYear(),
-   date.getMonth()+1,
-   date.getDate(),
-   date.getHours(),
-   date.getMinutes(),
- ]
+var server = net.createServer(
+  function (socket) {
+   var date = new Date()
+   date = [
+     date.getFullYear(),
+     date.getMonth()+1,
+     date.getDate(),
+     date.getHours(),
+     date.getMinutes(),
+   ]
 
- date = date.map(function(num){
-   if (num.toString().length == 1){
-     return "0"+ num
-   } else {
-     return num
-   }
- })
+   date = date.map(function(num){
+     if (num.toString().length == 1){
+       return "0"+ num
+     } else {
+       return num
+     }
+   })
 
- var dateTime =  date[0] + "-" + date[1] + "-" + date[2] + " " + date[3] + ":" + date[4]
- socket.write(dateTime)
- socket.end()
-})
+   var dateTime =  date[0] + "-" + date[1] + "-" + date[2] + " " + date[3] + ":" + date[4]
+   socket.end(dateTime)
+
+  }
+)
 server.listen(port)
