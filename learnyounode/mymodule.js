88c7d9e2cb248e1c18bb8033fd0c526e83207399
Camcan <mr.campbell.hawkes@gmail.com>
2016-04-06 22:56:56 +1200

    #9 Juggling Async. Wrong Month output from node on my machine. Strange

--- learnyounode/mymodule.js
+++ learnyounode/mymodule.js
@@ -1,16 +1,1 @@
-module.exports = function(address){
 
-  var http = require('http')
-  var bl= require('bl')
-
-  http.get(address, function(response) {
-    response.pipe(bl(function(err, data){
-      var fileString = data.toString()
-      console.log(fileString)
-    }))
-    response.on('error', function(error){
-      callback(error, null)
-    }
-  )}
-  )
-}
--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,7 +1,30 @@
-var getReq = require('./mymodule.js')
-var addresses = [process.argv[2], process.argv[3], process.argv[4]]
-for (var i = 0; i < addresses.length; i++){
-  getReq(addresses[i])
-}
+
+var port = process.argv[2]
 
 
+
+console.log(port)
+var net = require('net')
+var server = net.createServer(function (socket) {
+ var date = new Date
+ date = [
+   date.getFullYear(),
+   date.getMonth()+1,
+   date.getDate(),
+   date.getHours(),
+   date.getMinutes(),
+ ]
+
+ date = date.map(function(num){
+   if (num.toString().length == 1){
+     return "0"+ num
+   } else {
+     return num
+   }
+ })
+
+ var dateTime =  date[0] + "-" + date[1] + "-" + date[2] + " " + date[3] + ":" + date[4]
+ socket.write(dateTime)
+ socket.end()
+})
+server.listen(port)
