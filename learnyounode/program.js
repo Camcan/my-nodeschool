a217db2c1ccfa2e7d60f49894d1cc00f0072dd65
Camcan <mr.campbell.hawkes@gmail.com>
2016-04-06 23:59:36 +1200

    #11 Complete; code copied from others' soutions; instructions impenetrable.

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,32 +1,10 @@
+var http = require('http')
+var fs = require('fs')
 
 var port = process.argv[2]
+var file = process.argv[3]
 
-
-
-console.log(port)
-var net = require('net')
-var server = net.createServer(
-  function (socket) {
-   var date = new Date()
-   date = [
-     date.getFullYear(),
-     date.getMonth()+1,
-     date.getDate(),
-     date.getHours(),
-     date.getMinutes(),
-   ]
-
-   date = date.map(function(num){
-     if (num.toString().length == 1){
-       return "0"+ num
-     } else {
-       return num
-     }
-   })
-
-   var dateTime =  date[0] + "-" + date[1] + "-" + date[2] + " " + date[3] + ":" + date[4]
-   socket.end(dateTime)
-
-  }
-)
-server.listen(port)
+http.createServer(function(req, res){
+  res.writeHead(200, {'content-type' : 'text/plain' })
+  fs.createReadStream(file).pipe(res)
+}).listen(port)
