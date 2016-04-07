6fe50f195ff14ee4cf877b29f09d8e1bfffd4cbd
Camcan <mr.campbell.hawkes@gmail.com>
2016-04-07 15:07:15 +1200

    #12 Uppercaser server; Piped.

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,10 +1,14 @@
 var http = require('http')
-var fs = require('fs')
+var map = require('through2-map')
 
 var port = process.argv[2]
-var file = process.argv[3]
+
+var upperCase = map(function (chunk) {
+ return chunk.toString().toUpperCase()
+})
 
 http.createServer(function(req, res){
-  res.writeHead(200, {'content-type' : 'text/plain' })
-  fs.createReadStream(file).pipe(res)
+  if (req.method == 'POST'){
+    req.pipe(upperCase).pipe(res)
+  }
 }).listen(port)
