31136179d3d6823d1b5cab5a6f01a86bde67d5dc
Camcan <mr.campbell.hawkes@gmail.com>
2016-04-08 16:35:54 +1200

    #13 HTTP Server Complete.

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,14 +1,42 @@
 var http = require('http')
+var url = require('url')
 var map = require('through2-map')
 
 var port = process.argv[2]
 
-var upperCase = map(function (chunk) {
- return chunk.toString().toUpperCase()
-})
+function getUnixTimestamp(string){
+  return { unixtime: Date.parse(string) }
+}
 
-http.createServer(function(req, res){
-  if (req.method == 'POST'){
-    req.pipe(upperCase).pipe(res)
+function timeObject(string) {
+  var date = new Date(Date.parse(string));
+
+  return {
+    hour: date.getHours(),
+    minute: date.getMinutes(),
+    second: date.getSeconds()
+  };
+}
+
+
+
+var handleRequest = function(req, res){
+  var urlObj = url.parse(req.url, true)
+  var path = urlObj.pathname
+  var timeString = urlObj.query.iso
+  var toReturn = undefined
+  if (path === '/api/unixtime'){
+    toReturn = getUnixTimestamp(timeString)
+  } else if (path === '/api/parsetime'){
+    toReturn = timeObject(timeString)
   }
-}).listen(port)
+  if (toReturn) {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(toReturn))
+  } else {
+    res.writeHead(404)
+    res.end()
+  }
+}
+
+http.createServer(handleRequest).listen(port)
